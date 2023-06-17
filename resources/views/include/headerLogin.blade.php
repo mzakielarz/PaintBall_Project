7473816da855ae42bc60fc6f5c82000e9156ece8
Michał Zakielarz <michzack99@gmail.com>
2023-06-17 17:23:25 +0200

    navbar fixed

--- resources/views/include/headerLogin.blade.php
+++ resources/views/include/headerLogin.blade.php
@@ -3,16 +3,14 @@
 </head>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Paintballers</a>
+        <a class="navbar-brand" href="{{ route('index') }}">Paintballers</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
             aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse pt-0" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('index') }}">Home</a>
-                </li>
+
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('players') }}">Players</a>
                 </li>
