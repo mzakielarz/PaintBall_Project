9b79f2dd4c3853fff0e50e4bb8e01d861800f07b
Dominik Moskal <dominik150802@interia.pl>
2023-06-22 17:23:13 +0200

    carousel edit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -10,29 +11,37 @@
 
 
     <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
         }
-      }
-      body{
-        background-color: rgb(51, 48, 48)!important;
-        color: white !important;
-      }
 
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+
+        body {
+            background-color: rgb(51, 48, 48) !important;
+            color: white !important;
+        }
+
+        .carousel-image {
+            max-width: 100%;
+            height: auto;
+            object-fit: cover;
+        }
     </style>
 
 
-  </head>
-  <body>
+</head>
+
+<body>
+
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
         @include('include.headerLogin')
@@ -42,140 +51,151 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 
 
-<main>
+    <main>
 
-    <div id="carouselExampleCaptions" class="carousel slide">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="img/team1.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block" >
-                    <div  style="background-color: rgba(0, 0, 0, 0.4);">
-                        <h5>Experience the Thrill of Paintball</h5>
+        <div id="carouselExampleCaptions" class="carousel slide">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/team1.jpg" class="carousel-image" alt="...">
+                    <div class="carousel-caption d-none d-md-block" style="background-color: rgba(0, 0, 0, 0.4)">
+                        <h4>Experience the Thrill of Paintball</h4>
                         <p>Engage in adrenaline-pumping battles and strategic gameplay with our paintball experiences.</p>
                     </div>
                 </div>
-            </div>
-            <div class="carousel-item">
-                <img src="img/team2.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Unleash Your Competitive Spirit</h5>
-                    <p>Compete with friends and test your skills in intense paintball matches.</p>
+                <div class="carousel-item">
+                    <img src="img/team2.jpg" class="carousel-image" alt="...">
+                    <div class="carousel-caption d-none d-md-block" style="background-color: rgba(0, 0, 0, 0.4)">
+                        <h4>Unleash Your Competitive Spirit</h4>
+                        <p>Compete with friends and test your skills in intense paintball matches.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="img/soldier.jpg" class="carousel-image" alt="...">
+                    <div class="carousel-caption d-none d-md-block" style="background-color: rgba(0, 0, 0, 0.4)">
+                        <h4>Experience the Excitement of Paintball Action</h4>
+                        <p>Immerse yourself in the action-packed world of paintball and create lasting memories.</p>
+                    </div>
                 </div>
             </div>
-            <div class="carousel-item">
-                <img src="img/soldier.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Experience the Excitement of Paintball Action</h5>
-                    <p>Immerse yourself in the action-packed world of paintball and create lasting memories.</p>
-                </div>
-            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
 
 
-  <!-- Marketing messaging and featurettes
+
+        <!-- Marketing messaging and featurettes
   ================================================== -->
-  <!-- Wrap the rest of the page in another container to center all the content. -->
+        <!-- Wrap the rest of the page in another container to center all the content. -->
 
-  <div class="container marketing">
+        <div class="container marketing">
 
-    <!-- Three columns of text below the carousel -->
-    <div class="row">
-      <div class="col-lg-4">
-        <img class="rounded-circle" width="140" height="140" src="img/circle1.jpg">
-        <h2 style="color: yellow">Scenarios</h2>
-        <p >Experience the thrill of paintball with our exhilarating game scenarios and state-of-the-art equipment.</p>
+            <!-- Three columns of text below the carousel -->
+            <div class="row">
+                <div class="col-lg-4">
+                    <img class="rounded-circle" width="140" height="140" src="img/circle1.jpg">
+                    <h2 style="color: yellow">Scenarios</h2>
+                    <p>Experience the thrill of paintball with our exhilarating game scenarios and state-of-the-art
+                        equipment.</p>
 
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
-        <img class="rounded-circle" width="140" height="140" src="img/circle2.jpg">
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img class="rounded-circle" width="140" height="140" src="img/circle2.jpg">
 
-        <h2 style="color: yellow">Challenge</h2>
-        <p>Challenge your friends to intense paintball matches and see who emerges as the ultimate champion.</p>
+                    <h2 style="color: yellow">Challenge</h2>
+                    <p>Challenge your friends to intense paintball matches and see who emerges as the ultimate champion.
+                    </p>
 
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
-        <img class="rounded-circle" width="140" height="140" src="img/circle3.jpg">
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img class="rounded-circle" width="140" height="140" src="img/circle3.jpg">
 
-        <h2 style="color: yellow">Competetive</h2>
-        <p>Unleash your competitive spirit and experience the excitement of paintball action like never before.</p>
+                    <h2 style="color: yellow">Competetive</h2>
+                    <p>Unleash your competitive spirit and experience the excitement of paintball action like never
+                        before.</p>
 
-      </div><!-- /.col-lg-4 -->
-    </div><!-- /.row -->
-
-
-    <!-- START THE FEATURETTES -->
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7">
-        <h2 class="featurette-heading" style="color: yellow">Experience the Adrenaline Rush. Get in the game.</h2>
-        <p class="lead">Engage in heart-pounding battles and navigate thrilling paintball scenarios that will keep you on the edge of your seat.</p>
-      </div>
-      <div class="col-md-5">
-        <img class="featurette-image img-fluid mx-auto" width="500" height="500" src="img/bottom1.jpg">
-
-      </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading" style="color: yellow">Test Your Skills and Strategy. Victory awaits.</h2>
-        <p class="lead">Showcase your tactical prowess and teamwork as you engage in thrilling paintball battles with friends and opponents.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <img class="featurette-image img-fluid mx-auto" width="500" height="500" src="img/bottom2.jpg">
-
-      </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7">
-        <h2 class="featurette-heading" style="color: yellow">Create Unforgettable Memories. Capture the moment.</h2>
-        <p class="lead">Immerse yourself in the thrilling world of paintball and create lasting memories filled with excitement, laughter, and camaraderie.</p>
-      </div>
-      <div class="col-md-5">
-        <img class="featurette-image img-fluid mx-auto" width="500" height="500" src="img/bottom3.jpg">
-
-      </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <!-- /END THE FEATURETTES -->
-
-  </div><!-- /.container -->
+                </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
 
 
-  <!-- FOOTER -->
-  <footer class="container">
-    <p class="float-end"><a href="#">Back to top</a></p>
-    <p>&copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-  </footer>
-</main>
+            <!-- START THE FEATURETTES -->
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading" style="color: yellow">Experience the Adrenaline Rush. Get in the
+                        game.</h2>
+                    <p class="lead">Engage in heart-pounding battles and navigate thrilling paintball scenarios that
+                        will keep you on the edge of your seat.</p>
+                </div>
+                <div class="col-md-5">
+                    <img class="featurette-image img-fluid mx-auto" width="500" height="500" src="img/bottom1.jpg">
+
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7 order-md-2">
+                    <h2 class="featurette-heading" style="color: yellow">Test Your Skills and Strategy. Victory awaits.
+                    </h2>
+                    <p class="lead">Showcase your tactical prowess and teamwork as you engage in thrilling paintball
+                        battles with friends and opponents.</p>
+                </div>
+                <div class="col-md-5 order-md-1">
+                    <img class="featurette-image img-fluid mx-auto" width="500" height="500" src="img/bottom2.jpg">
+
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading" style="color: yellow">Create Unforgettable Memories. Capture the
+                        moment.</h2>
+                    <p class="lead">Immerse yourself in the thrilling world of paintball and create lasting memories
+                        filled with excitement, laughter, and camaraderie.</p>
+                </div>
+                <div class="col-md-5">
+                    <img class="featurette-image img-fluid mx-auto" width="500" height="500"
+                        src="img/bottom3.jpg">
+
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <!-- /END THE FEATURETTES -->
+
+        </div><!-- /.container -->
+
+
+        <!-- FOOTER -->
+        <footer class="container">
+            <p class="float-end"><a href="#">Back to top</a></p>
+            <p>&copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a
+                    href="#">Terms</a></p>
+        </footer>
+    </main>
 
 </body>
+
 </html>
